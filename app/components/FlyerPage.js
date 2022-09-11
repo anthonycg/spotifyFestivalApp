50e7d764194f2063eaa74a08b8b1e8e8e4d31c75
Anthony <anthonygibson24@gmail.com>
2022-09-11 14:43:04 -0500

    flyer page complete, ready to setup spotify api

--- app/components/FlyerPage.js
+++ app/components/FlyerPage.js
@@ -1,39 +1,62 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  StyleSheet,
+    Image
+} from "react-native";
 
 const FlyerPage = () => {
+  const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      width: null,
+      height: null,
+      resizeMode: "stretch",
+    },
+  });
 
-    return (
-        <SafeAreaView style={{
-            flex: 1,
-            backgroundColor: "red",
-        }}>
-            <View style={{
-                flex: .65,
-                backgroundColor: "tomato",
-                alignItems: "center"
-            }}>
-                <Text>Festival Name</Text>
-            </View>
-            <View style={{
-                flex: .10,
-                backgroundColor: "blue", 
-                alignItems: "center",
-                justifyContent: "center"
-            }}>
-                <Text style={{
-                    fontSize: 26,
-                    color: "white"
-                }}>Select an Artist:</Text>
-            </View>
-            <View style={{
-                flex: .35,
-                backgroundColor: "gold", 
-                alignItems: "center"
-            }}>
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "red",
+      }}
+    >
+    <View style={{flex: 1, resizeMode: "contain"}}>
+      <Image
+        style={styles.background}
+        source={require("../assets/ACL.png")}
+      ></Image>
+    </View>
+      <View
+        style={{
+          flex: 0.1,
+          backgroundColor: "blue",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 26,
+            color: "white",
+          }}
+        >
+          Select an Artist:
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 0.35,
+          backgroundColor: "gold",
+          alignItems: "center",
+        }}
+      ></View>
+    </SafeAreaView>
+  );
+};
 
 export default FlyerPage;
