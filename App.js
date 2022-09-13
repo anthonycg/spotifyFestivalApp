ee4d9712bf7cf89ba2defb091154813953da18f8
Anthony <anthonygibson24@gmail.com>
2022-09-12 23:58:07 -0500

    Picker skeleton built

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 import Screen from "./app/components/Screen";
 import HomePage from "./app/components/HomePage"
 import FlyerPage from "./app/components/FlyerPage";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
   return (
-    <FlyerPage/>
+    <AppPicker/>
   );
 }
 
