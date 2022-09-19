ff2592112813820e8fd7c1783f1714e49248fa8d
Anthony <anthonygibson24@gmail.com>
2022-09-18 20:11:17 -0500

    edited login screen

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
-                    name="Login"
+                    name=" "
                     component={SpotifyComponent}
                     options={{
                         headerStyle: {
